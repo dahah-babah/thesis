b7e3a0111c9b919c871a63d73c526cee66c86cf1
Daria Lobanova <daria_lobanova@epam.com>
2020-05-01 21:50:47 +0300

    Added BreadcrumbMap paths

--- thesis/src/components/uui/Breadcrumb/Breadcrumb.tsx
+++ thesis/src/components/uui/Breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Breadcrumb as BreadcrumbAntd } from 'antd';
-import { UserOutlined, DoubleRightOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 import './Breadcrumb.module.less';
 import { User, Student, Teacher } from '../../../types/types';
 import { inject, observer } from 'mobx-react';
@@ -15,23 +15,35 @@ interface Route {
     }>;
 }
 
-const { Item } = BreadcrumbAntd;
+const { Item, Separator } = BreadcrumbAntd;
 
 @inject('userStore')
 @observer
 export class Breadcrumb extends React.Component<Route | any> {
     //use dropdowns in courses->[coursenames]->works->[worknames] 
-    // home for student = not completed course
+
+    private renderDynamicItems = (): React.ReactNode => {
+        return (
+            <>
+                <Item>home</Item>
+                <Separator>/</Separator>
+            </>
+        );
+    };
 
     private renderItems = (): React.ReactNode => {
         if (this.props.userStore.user) {
             return (
-                <Item href={`/user/${this.props.userStore.user.id}/home`}>
-                    <UserOutlined />
-                    <span>
-                        {this.props.userStore.user.username}    
-                    </span>
-                </Item>
+                <>
+                    <Item href={`/user/${this.props.userStore.user.id}/home`}>
+                        <UserOutlined />
+                        <span>
+                            {this.props.userStore.user.username}    
+                        </span>
+                    </Item>
+                    <Separator>:</Separator>
+                    {this.renderDynamicItems()}
+                </>
             );
         } else {
             return null;
@@ -41,7 +53,7 @@ export class Breadcrumb extends React.Component<Route | any> {
     render(): React.ReactChild {
         return (
             <BreadcrumbAntd
-                separator={<DoubleRightOutlined />}
+                separator={""}
             >
                 {this.renderItems()}
             </BreadcrumbAntd>
--- thesis/src/components/uui/Breadcrumb/BreadcrumbMap.ts
+++ thesis/src/components/uui/Breadcrumb/BreadcrumbMap.ts
@@ -1,3 +1,6 @@
 export const breadcrumbNmaeMap = {
-    'user/:userId/courses': 'courses',
+    'user/:userId/home': 'home',
+    'user/:userId/courses/:courseId/edit': 'courses/:courseId',
+    'user/:userId/courses/:courseId/works/new': 'courses/:courseId/works/new',
+    'user/:userId/courses/:courseId/works/:workId/edit': 'courses/:courseId/tasks/:taskId'
 };

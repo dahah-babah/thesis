627ed1e47b4ecfddbee1739261145019b902eb3f
Daria Lobanova <daria_lobanova@epam.com>
2020-05-02 18:33:04 +0300

    Added Breadcrumb (does not work)

--- thesis/src/components/uui/Breadcrumb/Breadcrumb.tsx
+++ thesis/src/components/uui/Breadcrumb/Breadcrumb.tsx
@@ -4,6 +4,8 @@ import { UserOutlined } from '@ant-design/icons';
 import './Breadcrumb.module.less';
 import { User, Student, Teacher } from '../../../types/types';
 import { inject, observer } from 'mobx-react';
+import { breadcrumbNameMap } from './BreadcrumbMap';
+import { pathToRegexp } from 'path-to-regexp';
 
 interface Route {
     user: User | Student | Teacher;
@@ -20,13 +22,43 @@ const { Item, Separator } = BreadcrumbAntd;
 @inject('userStore')
 @observer
 export class Breadcrumb extends React.Component<Route | any> {
-    //use dropdowns in courses->[coursenames]->works->[worknames] 
+    //DO dropdowns in courses->[coursenames]->works->[worknames] 
+
+    // FIX breadcrumb + breadcrumbNameMap
+    // works only after reload
+
+    // FIX key property
 
     private renderDynamicItems = (): React.ReactNode => {
+        const location = window.location;
+        const pathSnippets = location.pathname.split('/').filter(i => i);
+        const extraBreadcrumbItems: any[] = [];
+
+        pathSnippets.forEach((_, index: number) => {
+            const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+            
+            Object.keys(breadcrumbNameMap).forEach((item: string) => {
+                
+                if (pathToRegexp(item).test(url)) { 
+                    console.log(breadcrumbNameMap[item]);
+                                     
+                    extraBreadcrumbItems.push(
+                        <Item key={url} href={url}>
+                            {breadcrumbNameMap[item]}
+                        </Item>
+                    );
+                }
+            });
+        });
+
+        const breadcrumbItems = [(
+            <>
+            </>
+        )].concat(extraBreadcrumbItems);
+        
         return (
             <>
-                <Item>home</Item>
-                <Separator>/</Separator>
+                {breadcrumbItems}
             </>
         );
     };
@@ -35,7 +67,7 @@ export class Breadcrumb extends React.Component<Route | any> {
         if (this.props.userStore.user) {
             return (
                 <>
-                    <Item href={`/user/${this.props.userStore.user.id}/home`}>
+                    <Item key={'home'} href={`/user/${this.props.userStore.user.id}/home`}>
                         <UserOutlined />
                         <span>
                             {this.props.userStore.user.username}    
--- thesis/src/components/uui/Breadcrumb/BreadcrumbMap.ts
+++ thesis/src/components/uui/Breadcrumb/BreadcrumbMap.ts
@@ -1,6 +1,5 @@
-export const breadcrumbNmaeMap = {
-    'user/:userId/home': 'home',
-    'user/:userId/courses/:courseId/edit': 'courses/:courseId',
-    'user/:userId/courses/:courseId/works/new': 'courses/:courseId/works/new',
-    'user/:userId/courses/:courseId/works/:workId/edit': 'courses/:courseId/tasks/:taskId'
+export const breadcrumbNameMap = {
+    '/user/:userId/courses/:courseId/works/:workId/edit': 'courses/:courseId/tasks/:taskId',
+    '/user/:userId/courses/:courseId/works/new': 'courses/:courseId/works/new',
+    '/user/:userId/courses/:courseId/edit': 'courses/:courseId'
 };
